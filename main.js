0d43ff6ea22f71bc5fb63f6736f36e474c30ac64
franlasso <francialasso@gmail.com>
2025-04-22 09:51:16 -0500

    subiendo trabajo

---
+++ main.js
@@ -0,0 +1,11 @@
+
+let calcular =document.getElementById("btn");
+let entrada = document.getElementById("entrada");
+let numero = 0;
+entrada.addEventListener("input", (evento)=>{
+    numero = evento.target.value;
+    console.log(numero);
+});
+calcular .addEventListener("click", ()=>[
+    console.log("calcular")
+])

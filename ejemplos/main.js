3d1d5ec6227a18243c91e6adbca5210ae5d89fc1
franlasso <francialasso@gmail.com>
2025-04-23 11:10:10 -0500

    actualizacion trabajos miercoles

---
+++ ejemplos/main.js
@@ -0,0 +1,28 @@
+// let j =0;
+// do {
+//     console.log(j);
+//     j++;
+// } while (j < 5)
+
+// let factorial = 1
+// for (let i = 1; i <= 5; i=i+1){
+//     factorial = factorial*i
+//     console.log("iteracion", i)
+// }
+//     console.log(factorial)
+
+    let frutas = ["manzana", "banana", "pera", "naranja"];
+    console.log(frutas[0]); //"manzana"
+    console.log(frutas[2]); //"pera"
+
+    frutas[1] ="kiwi";
+    console.log(frutas);//["manzana", "kiwi", "pera"]
+
+    console.log(frutas.length)
+
+    for (let i=0; i<frutas.length;i++){
+        console.log(frutas[i])
+    }
+
+
+    
